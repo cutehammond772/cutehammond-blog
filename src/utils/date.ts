537741bda389e607d65a322267ee6426c55c465f
cutehammond772 <cutehammond772@gmail.com>
2024-03-09 21:02:43 +0900

    fix: 날짜가 잘못 표기되는 현상 수정

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,3 @@
 export function convertDate(date: Date) {
-  return `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDay()}일`;
+  return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
 }
