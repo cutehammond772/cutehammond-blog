14c1e35a3a3b4aa07f8c001f4c19521c555c6bbd
LEE\JHL <cutehammond772@gmail.com>
2024-03-13 22:46:06 +0900

    fix: 가장 최근에 작성된 글이 먼저 오도록 수정

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -4,19 +4,24 @@ import { list, load } from "@/utils/article/github";
 import ArticleCard from "./components/ArticleCard";
 
 export default async function Page() {
-  const articleList = await list({});
+  const articleTitles = await list({});
 
   // 모든 글 목록이다.
-  // TODO: title? entry? 명칭을 확실히 정하기
-  const entries = await Promise.all(
-    articleList.entries.map((entry) => load({ title: entry }))
+  const articles = await Promise.all(
+    articleTitles.entries.map((title) => load({ title }))
+  );
+
+  // 날짜 순으로 정렬한다. (가장 늦게 나온 게시글이 먼저 오도록 한다.)
+  articles.sort(
+    (a, b) =>
+      new Date(b.createdDate).getTime() - new Date(a.createdDate).getTime()
   );
 
   return (
     <>
       <div className="f3-bold mb-8 mt-24 px-4 md:mt-48 md:px-0">모든 글</div>
       <div className="grid grid-flow-row grid-cols-1 gap-4 xl:grid-cols-2">
-        {entries.map(({ title, createdDate, tag }) => (
+        {articles.map(({ title, createdDate, tag }) => (
           <ArticleCard
             title={title}
             createdDate={convertDate(createdDate)}
